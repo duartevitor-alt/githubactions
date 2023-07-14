b6cb384934437ab01097da48ed772bab8d9bdec4
duartevitu-alt <vitordduarte95@gmail.com>
2023-07-14 15:01:49 +0100

    Creating venv

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+import logging
+from datetime import datetime
+
+import requests
+
+logging.basicConfig(level=logging.INFO)
+
+
+def requests_url(url: str, output: str) -> None:
+    URL: str = url
+    response = requests.get(url=URL).json()
+    logging.info(response)
+
+    with open(output, 'w') as file:
+        file.write(json.dumps(response))
+
+
+if __name__ == "__main__":
+    requests_url(
+        "https://dummyjson.com/products",
+        f"dummy_json_{datetime.now():%Y-%m-%d_%H_%M}.json"
+    )
